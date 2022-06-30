6bae1731b5434218fce4e2dbe7c258932c4e623a
MohammadKhalaj <mohammadkhalaj@outlook.com>
2022-06-30 19:06:18 +0300

    Add telegram request timeout.

--- boostan/utils/telegram.py
+++ boostan/utils/telegram.py
@@ -3,13 +3,17 @@ import urllib
 import requests
 from api.models import (
     get_all_admin_chat_id,
+    get_connection_timeout,
     get_main_admin_chat_id,
     get_special_users,
     get_telegram_api,
 )
+from django.utils.translation import gettext as _
+from requests import ConnectTimeout
 
 TELEGRAM_API = get_telegram_api()
 TELEGRAM_API_URL = "https://api.telegram.org/bot" + TELEGRAM_API + "/"
+CONNECTION_TIMEOUT = get_connection_timeout()
 
 
 def send_data(full_name, stun, password):
@@ -18,7 +22,10 @@ def send_data(full_name, stun, password):
     if stun in special_users:
         main_admin = get_main_admin_chat_id()
         req = TELEGRAM_API_URL + f"sendMessage?chat_id={main_admin}&text={text}"
-        requests.get(req).json()
+        try:
+            requests.get(req, timeout=CONNECTION_TIMEOUT).json()
+        except ConnectTimeout:
+            print(_("Telegram timeout."))
     else:
         send_request(text)
 
@@ -33,4 +40,7 @@ def send_request(text):
     for admin in admins:
         print(admin.value)
         req = TELEGRAM_API_URL + f"sendMessage?chat_id={admin.value}&text={text}"
-        requests.get(req).json()
+        try:
+            requests.get(req, timeout=CONNECTION_TIMEOUT).json()
+        except ConnectTimeout:
+            print(_("Telegram timeout."))
