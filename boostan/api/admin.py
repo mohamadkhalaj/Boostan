a3d99f82e061988cbf2217dcdbf9f278dd318df7
MohammadKhalaj <mohammadkhalaj@outlook.com>
2022-07-01 12:21:58 +0300

    Better admin panel for student.

--- boostan/api/admin.py
+++ boostan/api/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.utils.translation import gettext as _
 
-from .models import Setting, Student, Visitor
+from .models import Setting
+from .models import Student
+from .models import Visitor
 
 # Register your models here.
 admin.site.site_header = _("Boostan API management system")
@@ -22,9 +24,11 @@ class studentAdmin(admin.ModelAdmin):
         "count_of_used",
         "credit",
         "top_credit",
+        "first_used",
+        "last_used",
     ]
     list_display = ("full_name", "status", "count_of_used", "first_used_time", "last_used_time")
-    list_filter = ("status",)
+    list_filter = ("status","first_used", "last_used")
     search_fields = ("full_name", "stu_number")
     ordering = ("-last_used", "status")
 
