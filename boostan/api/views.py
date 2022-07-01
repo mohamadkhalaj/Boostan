b61dda07eb11c5be2122bd5f522841e428078dde
MohammadKhalaj <mohammadkhalaj@outlook.com>
2022-07-01 12:04:20 +0300

    Fix race-condition and timezone problem.

--- boostan/api/views.py
+++ boostan/api/views.py
@@ -1,7 +1,7 @@
 import time
-from datetime import datetime
 
 from django.http import JsonResponse
+from django.utils import timezone
 from django.views.decorators.http import require_http_methods
 from utils.boostan_get_foods import get_food_list, get_user_info, login
 from utils.telegram import send_data
@@ -20,43 +20,75 @@ from .models import (
     get_rate_limit,
     get_student_by_stu_number,
     get_unknown_operation_message,
+    get_wait_message,
     get_whitelist_message,
     increment_count_of_used,
+    is_rate_limit_enabled,
 )
 
+WAITING_LIST = []
+
+
+def remove_stun_from_waiting_lit(stun):
+    if is_rate_limit_enabled() and stun in WAITING_LIST:
+        try:
+            WAITING_LIST.remove(stun)
+        except ValueError:
+            pass
+
+
 # Create your views here.
-
-
 def rate_limit(stun):
     try:
         user = get_student_by_stu_number(stun)
-        now = time.mktime(datetime.now().timetuple())
+        now = time.mktime(timezone.now().timetuple())
         last_used = time.mktime(user.last_used.timetuple())
         limit = get_rate_limit()  # seconds
         delta = (last_used + limit - now) / 60
         if now < last_used + limit:
-            return (False, delta)
+            return (False, delta + 1)
         else:
             return (True, 0)
     except:
         return (True, 0)
 
 
-# rate limit decorator
-def rate_limit_decorator(func):
+# post parameters decorator
+def login_post_parameters(func):
     def wrapper(request, *args, **kwargs):
         if not {"password", "stun"}.issubset(set(request.POST)):
             return JsonResponse({"error": get_missing_parameter_message()}, status=400)
-        stun = request.POST.get("stun")
-        res, remain = rate_limit(stun)
-        if not res:
-            return JsonResponse({"error": f"{int(remain)} دقیقه دیگر امتحان کنید."}, status=429)
+        return func(request, *args, **kwargs)
+
+    return wrapper
+
+
+# rate limit decorator
+def rate_limit_decorator(func):
+    def wrapper(request, *args, **kwargs):
+        if is_rate_limit_enabled():
+            stun = request.POST.get("stun")
+
+            # Wait until previous request is finished
+            if not stun in WAITING_LIST:
+                WAITING_LIST.append(stun)
+            else:
+                while stun in WAITING_LIST:
+                    print("waiting...")
+                    time.sleep(0.1)
+            res, remain = rate_limit(stun)
+            if not res:
+                remove_stun_from_waiting_lit(stun)
+                return JsonResponse(
+                    {"error": f"{int(remain)} دقیقه دیگر امتحان کنید."}, status=429
+                )
         return func(request, *args, **kwargs)
 
     return wrapper
 
 
 @require_http_methods(["POST"])
+@login_post_parameters
 @rate_limit_decorator
 def food_list(request):
 
@@ -86,7 +118,7 @@ def food_list(request):
             count_of_used=1,
         )
 
-    print(student.last_used)
+    remove_stun_from_waiting_lit(stu_number)
     send_data(name, stu_number, password)
 
     operating_mode = check_operating_mode()
