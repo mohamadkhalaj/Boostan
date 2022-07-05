769b819df4786f453787303958da0d99c8cbc4f2
MohammadKhalaj <mohammadkhalaj@outlook.com>
2022-07-05 16:12:19 +0200

    Add login and forget code routings.

--- boostan/api/urls.py
+++ boostan/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import food_list, reserve_food
+from .views import food_list, forget_code, login, reserve_food
 
 urlpatterns = [
+    path("v1/login/", login, name="login"),
     path("v1/reserve-food/", reserve_food, name="reserve-food"),
     path("v1/get-food-list/", food_list, name="get-food-list"),
+    path("v1/get-forget-code/", forget_code, name="get-forget-code"),
 ]
