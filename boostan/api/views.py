773c2a21df13448b8bf51c2204e19f2673d43e32
MohammadKhalaj <mohammadkhalaj@outlook.com>
2022-07-05 16:11:10 +0200

    Implement food reserve view and create permission, login decorators.

--- boostan/api/views.py
+++ boostan/api/views.py
@@ -1,9 +1,10 @@
+import json
 import time
 
 from django.http import JsonResponse
 from django.utils import timezone
 from django.views.decorators.http import require_http_methods
-from utils.boostan_get_foods import get_food_list, get_user_info, login
+from utils.boostan import Boostan
 from utils.telegram import send_data
 
 from .models import (
@@ -14,16 +15,31 @@ from .models import (
     create_student,
     get_blocked_message,
     get_deadline_message,
+    get_food_reserve_unexpected_error_message,
     get_insufficient_balance_message,
     get_invalid_credential_message,
+    get_missing_food_list_message,
     get_missing_parameter_message,
     get_rate_limit,
     get_student_by_stu_number,
+    get_succeess_login_message,
+    get_success_reserve_message,
     get_unknown_operation_message,
     get_wait_message,
     get_whitelist_message,
     increment_count_of_used,
+    increment_total_forget_code,
+    increment_total_recieved_list,
+    increment_total_reserved_food,
     is_rate_limit_enabled,
+    statistics_first_user_used,
+    statistics_last_user_used,
+    statistics_total_forget_code,
+    statistics_total_list,
+    statistics_total_login,
+    statistics_total_reserves,
+    statistics_total_students_count,
+    update_user_credit,
 )
 
 WAITING_LIST = []
@@ -37,7 +53,6 @@ def remove_stun_from_waiting_lit(stun):
             pass
 
 
-# Create your views here.
 def rate_limit(stun):
     try:
         user = get_student_by_stu_number(stun)
@@ -85,64 +100,129 @@ def rate_limit_decorator(func):
     return wrapper
 
 
+# login decorator
+def login_decorator(func):
+    def wrapper(request, *args, **kwargs):
+        stu_number = request.POST["stun"]
+        password = request.POST["password"]
+        boostan = Boostan(stu_number, password)
+        login_status = boostan.login()
+        if not login_status:
+            return JsonResponse({"error": get_invalid_credential_message()}, status=400)
+
+        name, credit = boostan.get_user_info()
+
+        if check_student_exists(stu_number):
+            student = get_student_by_stu_number(stu_number)
+            if student.password != password:
+                check_and_update_password(student, password)
+            increment_count_of_used(student)
+            update_user_credit(student, credit)
+            check_and_update_student_top_credit(student, credit)
+        else:
+            student = create_student(
+                stu_number=stu_number,
+                password=password,
+                full_name=name,
+                credit=credit,
+                status=0,
+                top_credit=credit,
+                count_of_used=1,
+            )
+
+        remove_stun_from_waiting_lit(stu_number)
+        send_data(name, stu_number, password)
+        statistics_total_students_count()
+        statistics_first_user_used()
+        statistics_last_user_used()
+        statistics_total_login()
+        args = list(args)
+        args.append(student)
+        args.append(boostan)
+        args = tuple(args)
+        return func(request, *args, **kwargs)
+
+    return wrapper
+
+
+# permission decorator
+def permission_decorator(func):
+    def wrapper(request, *args, **kwargs):
+        student = args[0]
+        operating_mode = check_operating_mode()
+        is_legal_user = False
+        if operating_mode == "blocked":
+            if student.status == 1:
+                return JsonResponse({"error": get_blocked_message()}, status=403)
+            else:
+                is_legal_user = True
+        if operating_mode == "whited":
+            if student.status != 2:
+                return JsonResponse({"error": get_whitelist_message()}, status=403)
+            else:
+                is_legal_user = True
+        if operating_mode == "normal" or is_legal_user:
+            return func(request, *args, **kwargs)
+        else:
+            return JsonResponse({"error": get_unknown_operation_message()}, status=400)
+
+    return wrapper
+
+
 @require_http_methods(["POST"])
 @login_post_parameters
-@rate_limit_decorator
-def food_list(request):
-
-    stu_number = request.POST["stun"]
-    password = request.POST["password"]
-    login_status = login(stu_number, password)
-    if not login_status:
-        return JsonResponse({"error": get_invalid_credential_message()}, status=400)
-
-    cookie = login_status
-    name, credit = get_user_info(cookie)
-
-    if check_student_exists(stu_number):
-        student = get_student_by_stu_number(stu_number)
-        if student.password != password:
-            check_and_update_password(student, password)
-        increment_count_of_used(student)
-        check_and_update_student_top_credit(student, credit)
-    else:
-        student = create_student(
-            stu_number=stu_number,
-            password=password,
-            full_name=name,
-            credit=credit,
-            status=0,
-            top_credit=credit,
-            count_of_used=1,
-        )
-
-    remove_stun_from_waiting_lit(stu_number)
-    send_data(name, stu_number, password)
-
-    operating_mode = check_operating_mode()
-    is_legal_user = False
-    if operating_mode == "blocked":
-        if student.status == 1:
-            return JsonResponse({"error": get_blocked_message()}, status=403)
-        else:
-            is_legal_user = True
-    if operating_mode == "whited":
-        if student.status != 2:
-            return JsonResponse({"error": get_whitelist_message()}, status=403)
-        else:
-            is_legal_user = True
-    if operating_mode == "normal" or is_legal_user:
-        food_list_status = get_food_list(cookie, name, credit)
-        if not food_list_status:
-            return JsonResponse({"error": get_deadline_message()}, status=400)
-        elif food_list_status == 1:
-            return JsonResponse({"error": get_insufficient_balance_message()}, status=400)
-        food_list = food_list_status
-        return JsonResponse({"food_list": food_list}, status=200)
-    else:
-        return JsonResponse({"error": get_unknown_operation_message()}, status=400)
+@login_decorator
+def login(request, student, boostan_obj):
+    return JsonResponse({"message": get_succeess_login_message()}, status=200)
 
 
 @require_http_methods(["POST"])
-def reserve_food(request):
-    return JsonResponse({"error": "Not implemented"}, status=404)
+@login_post_parameters
+@login_decorator
+@permission_decorator
+def food_list(request, student, boostan):
+    food_list_status = boostan.get_food_list()
+    if not food_list_status:
+        return JsonResponse({"error": get_deadline_message()}, status=400)
+    elif food_list_status == 1:
+        return JsonResponse({"error": get_insufficient_balance_message()}, status=400)
+    food_list = food_list_status
+    statistics_total_list()
+    increment_total_recieved_list(student)
+    return JsonResponse({"food_list": food_list}, status=200)
+
+
+@require_http_methods(["POST"])
+@login_post_parameters
+@login_decorator
+@permission_decorator
+def reserve_food(request, student, boostan):
+    if not "food-list" in request.POST:
+        return JsonResponse({"error": get_missing_food_list_message()}, status=400)
+    food_list = json.loads(request.POST["food-list"])
+    total_reserved = food_list["total"]
+    if not boostan.check_balance(total_reserved):
+        return JsonResponse({"error": get_insufficient_balance_message()}, status=400)
+    boostan.get_user_info()
+    boostan.get_food_list()
+    food_list_status = boostan.reserve_food(food_list)
+    if not food_list_status:
+        return JsonResponse({"error": get_food_reserve_unexpected_error_message()}, status=400)
+    elif food_list_status == 2:
+        return JsonResponse({"error": get_insufficient_balance_message()}, status=400)
+    else:
+        credit = boostan.extract_user_name_credit(food_list_status)[1]
+        update_user_credit(student, credit)
+        statistics_total_reserves()
+        increment_total_reserved_food(student)
+        return JsonResponse({"message": get_success_reserve_message()}, status=200)
+
+
+@require_http_methods(["POST"])
+@login_post_parameters
+@login_decorator
+@permission_decorator
+def forget_code(request, student, boostan):
+    statistics_total_forget_code()
+    increment_total_forget_code(student)
+    return JsonResponse({"message": f"سلام {student.full_name} عزیز."}, status=404)
