3e31ddd36cce004489e97cd40893e670f00efd51
MohammadKhalaj <mohammadkhalaj@outlook.com>
2022-07-13 16:14:54 +0200

    Better login.

--- config/api_router.py
+++ config/api_router.py
@@ -1,6 +1,7 @@
 from api.urls import urlpatterns as api_urls
 from django.conf import settings
-from rest_framework.routers import DefaultRouter, SimpleRouter
+from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 
 from boostan.users.api.views import UserViewSet
 
@@ -13,5 +14,5 @@ router.register("users", UserViewSet)
 
 
 app_name = "api"
-urlpatterns = router.urls
-urlpatterns += api_urls
+# urlpatterns = router.urls
+urlpatterns = api_urls
--- config/urls.py
+++ config/urls.py
@@ -1,21 +1,23 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include
+from django.urls import path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView
+from drf_spectacular.views import SpectacularSwaggerView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    # path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path("food/", TemplateView.as_view(template_name="pages/food.html"), name="food"),
-    path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
+    # path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
-    path("users/", include("boostan.users.urls", namespace="users")),
-    path("accounts/", include("allauth.urls")),
+    # path("users/", include("boostan.users.urls", namespace="users")),
+    # path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
@@ -24,13 +26,13 @@ urlpatterns += [
     # API base url
     path("api/", include("config.api_router")),
     # DRF auth token
-    path("auth-token/", obtain_auth_token),
-    path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
-    path(
-        "api/docs/",
-        SpectacularSwaggerView.as_view(url_name="api-schema"),
-        name="api-docs",
-    ),
+    # path("auth-token/", obtain_auth_token),
+    # path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
+    # path(
+    #     "api/docs/",
+    #     SpectacularSwaggerView.as_view(url_name="api-schema"),
+    #     name="api-docs",
+    # ),
 ]
 
 if settings.DEBUG:
