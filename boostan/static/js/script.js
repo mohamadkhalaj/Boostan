9c9015e37d70eee27b965b420066c45040342bff
MohammadKhalaj <mohammadkhalaj@outlook.com>
2022-07-13 20:18:27 +0200

    Fix toggle password btn bug.

--- boostan/static/js/script.js
+++ boostan/static/js/script.js
@@ -27,29 +27,17 @@ var reserve_list = {'total':0, 'days':[]};
 var created_objects = [];
 
 
-var ShowPasswordToggle = document.querySelector("[type='password']");
-ShowPasswordToggle.onclick = function () {
-  document.querySelector("[type='password']").classList.add("input-password");
-  document.getElementById("toggle-password").classList.remove("d-none");
+const passwordInput = document.querySelector("[type='password']");
+const togglePasswordButton = document.getElementById("toggle-password");
+togglePasswordButton.addEventListener("click", togglePassword);
+function togglePassword() {
+	if (passwordInput.type === "password") {
+	  passwordInput.type = "text";
+	} else {
+	  passwordInput.type = "password";
+	}
+}
 
-  const passwordInput = document.querySelector("[type='password']");
-  const togglePasswordButton = document.getElementById("toggle-password");
-
-  togglePasswordButton.addEventListener("click", togglePassword);
-  function togglePassword() {
-    if (passwordInput.type === "password") {
-      passwordInput.type = "text";
-      togglePasswordButton.setAttribute("aria-label", "Hide password.");
-    } else {
-      passwordInput.type = "password";
-      togglePasswordButton.setAttribute(
-        "aria-label",
-        "Show password as plain text. " +
-          "Warning: this will display your password on the screen."
-      );
-    }
-  }
-};
 
 function create_days(){
 	let parent_day_cards = document.getElementById('day-cards');
