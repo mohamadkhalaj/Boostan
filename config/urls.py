6daaddd1a8ed55f2862e415b69be012de057602f
MohammadKhalaj <mohammadkhalaj@outlook.com>
2022-07-13 16:29:15 +0200

    Disabel DRF.

--- config/urls.py
+++ config/urls.py
@@ -10,14 +10,14 @@ from drf_spectacular.views import SpectacularSwaggerView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/food.html"), name="home"),
+    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path("food/", TemplateView.as_view(template_name="pages/food.html"), name="food"),
-    # path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
+    path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
-    # path("users/", include("boostan.users.urls", namespace="users")),
-    # path("accounts/", include("allauth.urls")),
+    path("users/", include("boostan.users.urls", namespace="users")),
+    path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
