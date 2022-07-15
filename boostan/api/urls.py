b248b96e6f558f5010bed3c218932c7342732b67
MohammadKhalaj <mohammadkhalaj@outlook.com>
2022-07-15 22:09:18 +0430

    Add app_name="boostan_api" and move v1 to main urls.py

--- boostan/api/urls.py
+++ boostan/api/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from .views import food_list, forget_code, get_sessions, login, logout, reserve_food
 
+app_name = "boostan_api"
 urlpatterns = [
-    path("v1/login/", login, name="login"),
-    path("v1/reserve-food/", reserve_food, name="reserve-food"),
-    path("v1/get-food-list/", food_list, name="get-food-list"),
-    path("v1/get-forget-code/", forget_code, name="get-forget-code"),
-    path("v1/logout/", logout, name="logout"),
-    path("v1/get-sessions/", get_sessions, name="get-sessions"),
+    path("login/", login, name="login"),
+    path("reserve-food/", reserve_food, name="reserve-food"),
+    path("get-food-list/", food_list, name="get-food-list"),
+    path("get-forget-code/", forget_code, name="get-forget-code"),
+    path("logout/", logout, name="logout"),
+    path("get-sessions/", get_sessions, name="get-sessions"),
 ]
