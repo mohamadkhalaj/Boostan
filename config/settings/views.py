f5ee3f409371c72ff44ccba40c3d922a40695f01
MohammadKhalaj <mohammadkhalaj@outlook.com>
2022-07-17 21:04:14 +0430

    Remove unused files.

--- config/settings/views.py
+++ config/settings/views.py
@@ -4,12 +4,12 @@ from api.models import get_403_message, get_404_message, get_500_message
 
 
 def page_not_found_view(request, exception):
-    return JsonResponse({"message": get_404_message()}, status=404)
+    return JsonResponse({"error": get_404_message()}, status=404)
 
 
 def handler500(request):
-    return JsonResponse({"message": get_500_message()}, status=500)
+    return JsonResponse({"error": get_500_message()}, status=500)
 
 
 def handler403(request, exception):
-    return JsonResponse({"message": get_403_message()}, status=403)
+    return JsonResponse({"error": get_403_message()}, status=403)
--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,8 @@
-from argparse import Namespace
-
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include
-from django.urls import path
-from django.views import defaults as default_views
+from django.urls import include, path
 from django.views.generic import TemplateView
-from drf_spectacular.views import SpectacularAPIView
-from drf_spectacular.views import SpectacularSwaggerView
-from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/food.html"), name="home"),
