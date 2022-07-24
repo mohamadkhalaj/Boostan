430474847d9f33ad11ccee529104ec467a280bc1
MohammadMahdi Khalaj <mohammadkhalaj@outlook.com>
2022-07-24 18:32:35 +0430

    Create home and food views.

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
+from config.views import food, home
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/food.html"), name="home"),
-    path("food/", TemplateView.as_view(template_name="pages/food.html"), name="food"),
+    path("", home, name="home"),
+    path("food/", food, name="food"),
     path(settings.ADMIN_URL, admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
