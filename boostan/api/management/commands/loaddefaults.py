91ce0aef42797485fcfbeda86a20221101f61904
MohammadMahdi Khalaj <mohammadkhalaj@outlook.com>
2022-07-24 15:54:35 +0430

    Update loaddefaults custom management command for template tags fixtures.

--- boostan/api/management/commands/loaddefaults.py
+++ boostan/api/management/commands/loaddefaults.py
@@ -1,7 +1,7 @@
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
-from api.models import Message, Setting
+from api.models import Message, Setting, TemplateTags
 
 
 class Command(BaseCommand):
@@ -22,3 +22,10 @@ class Command(BaseCommand):
             call_command("loaddata", fixtures_path + "messages.json")
         else:
             self.stdout.write("Skip loading data for message model.")
+        
+        template_tags_count = TemplateTags.objects.all().count()
+        if not template_tags_count:
+            self.stdout.write("Load template tags model data...")
+            call_command("loaddata", fixtures_path + "tags.json")
+        else:
+            self.stdout.write("Skip loading data for template tags model.")
