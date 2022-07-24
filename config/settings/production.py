db89a35c5413558ca0a650827a6999a192b77469
MohammadMahdi Khalaj <mohammadkhalaj@outlook.com>
2022-07-24 12:52:40 +0430

    Add sentry issue tracker for both django and js.

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,5 +1,7 @@
 from .base import *  # noqa
 from .base import env
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # GENERAL
 # ------------------------------------------------------------------------------
@@ -30,6 +32,23 @@ CACHES = {
         },
     }
 }
+
+# SENTRY ISSUE TRACKER CONFIGURATION
+sentry_sdk.init(
+    dsn=env("SENTRY_DSN"),
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 # APPS
 # ------------------------------------------------------------------------------
